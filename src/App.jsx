bee79947b7c9b1d4e6b646d314a31a8d8ad15ef1
gyseong <jsjs8667@naver.com>
2023-09-13 12:47:07 +0900

    mainpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-
+import { Menu } from "./components/Menu";
+import { ScrollControls,Scroll, Html } from "@react-three/drei";
+import { Interface } from "./components/Interface";
+import { useState } from "react";
+import { Cursor } from "./components/Cursor";
 function App() {
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={2} damping={0.1}>
+          <Experience menuOpened={menuOpened}/>
+          <Scroll html>
+            <Interface/>
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+      <Menu menuOpened={menuOpened} setMenuOpened={setMenuOpened}/>
+      <h1 style={{ position: 'absolute', top: 50, left: 40, fontSize: '15px', color: 'Black' }}>ChatDB</h1>
+      <Cursor/>
+    </>
+
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,38 @@
-import { OrbitControls } from "@react-three/drei";
+import {  useFrame } from "@react-three/fiber";
+import React, { useRef, useEffect } from "react";
+import { framerMotionConfig } from "../config";
+import { animate, useMotionValue } from "framer-motion";
 
-export const Experience = () => {
+export const Experience = (props) => {
+  const mesh = useRef();
+  const {menuOpened} = props;
+  const cameraPositionX = useMotionValue();
+  const cameraLookAtX = useMotionValue();
+
+  useEffect(() => {
+    animate(cameraPositionX, menuOpened ? -10 : 0, {
+      ...framerMotionConfig,
+    });
+    animate(cameraLookAtX, menuOpened ? 1 : 0, {
+      ...framerMotionConfig,
+    });
+  }, [menuOpened]);
+
+  useFrame((state) => {
+    state.camera.position.x = cameraPositionX.get();
+    state.camera.lookAt(cameraLookAtX.get(), 0, 0);
+  });
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.x += 0.01;
+      mesh.current.rotation.y += 0.01;
+    }
+  });
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
+      <mesh ref={mesh}>
         <boxGeometry />
         <meshNormalMaterial />
       </mesh>
